4a40784686af6d1949f333b30079690a9792e90a
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-07-07 21:35:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+for(let i=1; i<11; i++){
+    console.log(i);
+}
+
+for(let i=10; i>0; i--){
+    console.log(i);
+}
+
+let i=1;
+
+while(i<11){
+    console.log(i);
+    i++;
+}
+
+i--;
+
+while(i>0){
+    console.log(i);
+    i--;
+}
+
+i++;
+
+do{
+    console.log(i);
+    i++;
+}while(i<11)
+
+i--;
+
+do{
+    console.log(i);
+    i--;
+}while(i>0)
+
+let constnumbers= [1,2,3,4,5,6,7,8,9,10];
+
+for(let num of constnumbers){
+    console.log(num);
+}
+
